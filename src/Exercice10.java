181c714455531606f09807d89d398ab6f68c5afe
maxime <maxime.aumegeas@laplateforme.io>
2025-11-06 16:12:40 +0100

    modif 10 + ajout 11

--- src/Exercice10.java
+++ src/Exercice10.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class Exercice10 {
-    public static void premier (){
-        Scanner input = new Scanner(System.in);
-        System.out.println("entrer le numero pour faire votre choix: ");
-        System.out.println("1-Dire Bonjour");
-        System.out.println("2-Dire Au revoir");
-        System.out.println("3-Dire Quitter");
-        int valeur1 = input.nextInt();
+    public static void menu (){
+
+        int valeur1;
         do {
+            Scanner input = new Scanner(System.in);
+            System.out.println("entrer le numero pour faire votre choix: ");
+            System.out.println("1-Dire Bonjour");
+            System.out.println("2-Dire Au revoir");
+            System.out.println("3-Dire Quitter");
+            valeur1 = input.nextInt();
+
             if (valeur1 == 1) {
                System.out.println("Bonjour!!");
             }
@@ -23,6 +26,6 @@ public class Exercice10 {
         while ((valeur1 == 0) || (valeur1 > 3));
     }
     public static void main (String[] args) {
-        premier ();
+        menu ();
     }
 }
---
+++ src/Exercice11.java
@@ -0,0 +1,9 @@
+public class Exercice11 {
+    public static void premier (){
+
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
