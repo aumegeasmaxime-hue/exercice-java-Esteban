51d5fb8a8f9b44d0a5731d58506f8c839ea0fe4e
maxime <maxime.aumegeas@laplateforme.io>
2025-11-06 16:02:10 +0100

    modif 5

--- src/Exercice5.java
+++ src/Exercice5.java
@@ -4,7 +4,6 @@ public class Exercice5 {
     public static void damier (int valeur1) {
 
         for (int j = 0; j < valeur1; j++) {
-
             for (int a = 0; a < valeur1; a++) {
                 if ((j+a)%2==0){
                     System.out.print("#");
