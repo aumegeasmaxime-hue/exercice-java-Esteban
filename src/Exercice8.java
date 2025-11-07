1ef4e4984ab8ffdaf4fdc6c13a0a578184475e34
maxime <maxime.aumegeas@laplateforme.io>
2025-11-07 09:39:25 +0100

    ajout exo 8

--- src/Exercice8.java
+++ src/Exercice8.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 public class Exercice8 {
     public static void nombreDeChiffre (){
         Scanner input = new Scanner(System.in);
-        System.out.println("Entrer un nombres pour connaitre le nombre de chiffre le composant");
+        System.out.print("Entrer un nombres pour connaitre le nombre de chiffre le composant: ");
+
         int valeur1 = input.nextInt();
+        int length = 0;
+        int temp = 1;
 
-
-
-
-
-
-
+        while (temp <= valeur1) {
+            length++;
+            temp *= 10;
+        }
+        System.out.print(length);
     }
     public static void main (String[] args) {
         nombreDeChiffre ();
