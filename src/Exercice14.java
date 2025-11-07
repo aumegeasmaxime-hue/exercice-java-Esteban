453c36bb52628e3cb8ecf30be0784bfd6d0369b7
maxime <maxime.aumegeas@laplateforme.io>
2025-11-07 10:55:45 +0100

    modif exo 14

--- src/Exercice14.java
+++ src/Exercice14.java
@@ -1,6 +1,11 @@
 public class Exercice14 {
     public static void valeurTotal (){
-
+        int [] tab1 = {2,4,6,8,10};
+        int result = 1;
+        for (int i : tab1){
+            result *= i;
+        }
+        System.out.println(result);
     }
     public static void main (String[] args) {
         valeurTotal ();
