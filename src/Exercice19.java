b29e1d63c2d422e9d3463ae2c08ec705b8860834
maxime <maxime.aumegeas@laplateforme.io>
2025-11-07 16:03:13 +0100

    modif 19

--- src/Exercice19.java
+++ src/Exercice19.java
@@ -1,8 +1,11 @@
 public class Exercice19 {
-    public static void creationTableau () {
-
+    public static void reverseTableau () {
+        int [] tab = {70,60,50,40,30,20,10};
+        for (int i = tab.length-1 ; i >= 0 ; i--) {
+            System.out.print( tab [i]  + " ");
+        }
     }
     public static void main (String[] args) {
-
+        reverseTableau();
     }
 }
