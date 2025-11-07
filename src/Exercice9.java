d0122fa50ebfb174c7fcc7e39edcb3a945dc73f5
maxime <maxime.aumegeas@laplateforme.io>
2025-11-07 10:35:18 +0100

    modif exo 9

--- src/Exercice9.java
+++ src/Exercice9.java
@@ -3,24 +3,33 @@ import java.util.Scanner;
 public class Exercice9 {
     public static void premier (){
         Scanner input = new Scanner(System.in);
-        System.out.println("Entrer un numero");
+        System.out.print("Entrer un numero :  ");
         int valeur1 = input.nextInt();
-        boolean a = true;
-        while (a) {
+        while (valeur1 != 0) {
+            if ((valeur1 == 1)||(valeur1 == 3)||(valeur1 == 5)||(valeur1 == 7)){
+                System.out.println("Peut-être premier");
+                break;
+            }
             if (valeur1 % 2 == 0){
                 System.out.println("Pas premier");
+                break;
             }
             else if (valeur1 % 3 == 0){
                 System.out.println("Pas premier");
+                break;
             }
             else if (valeur1 % 5 == 0){
                 System.out.println("Pas premier");
+                break;
             }
             else if (valeur1 % 7 == 0){
                 System.out.println("Pas premier");
+                break;
             }
+
             else {
                 System.out.println("Peut-être premier");
+                break;
             }
         }
     }
