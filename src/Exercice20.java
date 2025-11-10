fee5894d291161d16522a01e15a7ab9f22f43fe7
maxime <maxime.aumegeas@laplateforme.io>
2025-11-10 10:19:30 +0100

    modif  20

--- src/Exercice20.java
+++ src/Exercice20.java
@@ -1,14 +1,33 @@
 import java.util.Scanner;
 
 public class Exercice20 {
-    public static void reverseTableau () {
-        int [] tab = {2,4,6,8,10,13,15};
-        Scanner input = new Scanner(System.in);
-        System.out.print("entrer le nombre recherché :  ");
+    public static void rechercheTableau () {
 
+            int [] tab = {2,4,6,8,10,13,15};
+            Scanner input = new Scanner(System.in);
+            System.out.print("entrer le nombre recherché :  ");
+            int nbRecherche = input.nextInt();
 
+            boolean present = false;
+
+            for (int x : tab) {
+                if (x == nbRecherche) {
+                    System.out.print("le nombre est dans le tableau a la position :  ");
+                    present = true;
+                }
+            }
+            if (present == false) {
+                System.out.println("le nombre n'est pas dans le tableau");
+            }
+            for (int i = 0; i < tab.length; i++) {
+                if (tab[i] == nbRecherche) {
+                    System.out.println(i);
+                }
+            }
     }
-    public static void main (String[] args) {
 
+
+    public static void main (String[] args) {
+        rechercheTableau();
     }
 }
